ef79023a20c3816932c6a81508f3fdec50083358
Judith Barrett <judith.barrett@gmail.com>
2014-12-11 23:27:47 -0700

    Added ‘users’ migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212061803) do
+ActiveRecord::Schema.define(version: 20141212062535) do
 
   create_table "clients", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "gender"
     t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
